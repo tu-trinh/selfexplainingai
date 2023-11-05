39a552b4d9ff4d0bc05b1de004949eb3ad614bc1
Tu Trinh <tutrinh@berkeley.edu>
2023-11-05 13:12:43 -0800

    fix edge cases

--- utils.py
+++ utils.py
@@ -125,3 +125,27 @@ def get_obs_desc(obs):
         description += f"Finally, you are holding {agent_obj}."
     
     return description
+
+def convert_response_to_action(resp):
+    if "forward" in resp or "1" in resp:
+        return 1
+    if "left" in resp or "2" in resp:
+        return 2
+    if "right" in resp or "3" in resp:
+        return 3
+    if "pick" in resp or "4" in resp:
+        return 4
+    if "put" in resp or "5" in resp:
+        return 5
+    if "unlock" in resp or "6" in resp:
+        return 6
+    if "open" in resp or "7" in resp:
+        return 7
+    if "close" in resp or "8" in resp:
+        return 8
+    return None
+
+def format_seconds(seconds):
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds))
